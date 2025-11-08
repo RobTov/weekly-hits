e0a2683973b5b5ca6f22722f4d70a9ae8b0f89f6
RobTov <tovelrob25@gmail.com>
2025-11-07 23:03:45 -0500

    fix: fixed cors conflicts and url typos

--- web/src/store/artist-store.ts
+++ web/src/store/artist-store.ts
@@ -5,25 +5,25 @@ import { create as createFn, get, update, deletefn } from "../services/api";
 export const useArtistStore = create<ArtistStore>()((set) => ({
   artists: [],
   getArtist: async () => {
-    const artists = await get("artist");
+    const artists = await get("artists/");
     return set((state) => ({ ...state, artists }));
   },
   createArtist: async (newArtist: Artist) => {
-    const artist = await createFn(newArtist, "artist");
+    const artist = await createFn(newArtist, "artists");
     return set((state) => ({
       ...state,
       artists: [...state.artists, artist],
     }));
   },
   updateArtist: async (updatedArtist: Artist) => {
-    const artist = await update(updatedArtist, "artist");
+    const artist = await update(updatedArtist, "artists");
     return set((state) => ({
       ...state,
       artists: state.artists.map((a) => (a.id === artist.id ? artist : a)),
     }));
   },
   deleteArtist: async (id: number) => {
-    await deletefn(id, "artist");
+    await deletefn(id, "artists");
     return set((state) => ({
       ...state,
       artists: state.artists.filter((artist) => id === artist.id),
